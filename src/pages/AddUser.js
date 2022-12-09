11d347023dcd39354a1779d96e6a998191113c1c
Jagadeesh-Kumar-Initial-Is-S <115778774+Jagadeesh-Kumar-Initial-Is-S@users.noreply.github.com>
2022-12-09 10:01:47 +0530

    Update AddUser.js

--- src/pages/AddUser.js
+++ src/pages/AddUser.js
@@ -33,7 +33,7 @@ function AddUser({ userList, setUserList }) {
             className="form-control"
             id="userName"
             name="userName"
-            placeholder="Name of the user"
+            placeholder="Enter the name of the user"
             value={userName}
             onChange={(e) => setUserName(e.target.value)}
           />
@@ -44,7 +44,7 @@ function AddUser({ userList, setUserList }) {
             className="form-control"
             id="userEmail"
             name="userEmail"
-            placeholder="Email address of the user"
+            placeholder="Enter the email address of the user"
             value={userEmail}
             onChange={(e) => setUserEmail(e.target.value)}
           />
@@ -55,7 +55,7 @@ function AddUser({ userList, setUserList }) {
             className="form-control"
             id="userPic"
             name="userPic"
-            placeholder="URL ( Uniform Resource Locator ) for the photo of the user"
+            placeholder="Enter the URL ( Uniform Resource Locator ) for the photo of the user"
             value={userPic}
             onChange={(e) => setUserPic(e.target.value)}
           />
